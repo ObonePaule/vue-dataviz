81cf2a800cafb7d61b86d889814a26ad8516be8c
ObonePaule <47106158+ObonePaule@users.noreply.github.com>
2020-12-28 02:25:35 +0100

    Integration of the map with leaflet

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,52 @@
 <template>
   <div class="home">
-    <h1>This is a home page</h1>
+    <div style="height: 350px;">
+    <div class="info" style="height: 15%">
+      <span>Center: {{ center }}</span>
+      <span>Zoom: {{ zoom }}</span>
+      <span>Bounds: {{ bounds }}</span>
+    </div>
+    <l-map
+      style="height: 80%; width: 100%"
+      :zoom="zoom"
+      :center="center"
+      @update:zoom="zoomUpdated"
+      @update:center="centerUpdated"
+      @update:bounds="boundsUpdated"
+    >
+      <l-tile-layer :url="url"></l-tile-layer>
+    </l-map>
+  </div>
   </div>
 </template>
+
+
+<script>
+import {LMap, LTileLayer} from 'vue2-leaflet';
+
+export default {
+  components: {
+    LMap,
+    LTileLayer,
+  },
+  data () {
+    return {
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      zoom: 3,
+      center: [47.413220, -1.219482],
+      bounds: null
+    };
+  },
+  methods: {
+    zoomUpdated (zoom) {
+      this.zoom = zoom;
+    },
+    centerUpdated (center) {
+      this.center = center;
+    },
+    boundsUpdated (bounds) {
+      this.bounds = bounds;
+    }
+  }
+}
+</script>
