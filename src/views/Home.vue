34ea4466142fca36f25432d9e9c37a02a9bdc76d
ObonePaule <47106158+ObonePaule@users.noreply.github.com>
2020-12-29 02:51:47 +0100

    💄 Adding popup with content for station

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,6 +15,7 @@
             :icon-anchor="iconAnchor"
             icon-url="https://www.ija-lille.fr/wpress/wp-content/uploads/2018/01/map-marker-icon.png"
           />
+          <l-popup :content="station.popupcontent"/>
         </l-marker>
         <l-tile-layer :url="url"></l-tile-layer>
       </l-map>
@@ -23,25 +24,32 @@
 </template>
 
 <script>
-import { LMap, LTileLayer, LMarker, LIcon } from "vue2-leaflet";
+import { LMap, LTileLayer, LMarker, LIcon, LPopup } from "vue2-leaflet";
 
 export default {
   components: {
     LMap,
     LTileLayer,
     LMarker,
-    LIcon
+    LIcon,
+    LPopup
   },
   data() {
     return {
       stations: [
         {
+          name:"aa",
+          popupcontent:"Gare de saint lazare <br/><a href='google.com'>ah oui</a><a href='google.com'>ok</a>",
           latlng: [48.8926013, 2.2339234]
         },
         {
+          name:"aa",
+          popupcontent:"Gare de la defense<br/><a href='google.com'>ah oui</a><a href='google.com'>ok</a>",
           latlng: [48.872829842, 2.321332048]
         },
         {
+          name:"aa",
+          popupcontent:"Gare de Bercy <br/><a href='google.com'>ah oui</a><a href='google.com'>ok</a>",
           latlng: [48.8391474, 2.3805528]
         },
       ],
